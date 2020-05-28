f9620cf81cbf33e04dbed088a97579cc4eddb718
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-05-28 10:27:12 -0700

    Add README.md, package.json, and start my index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,28 @@
-const questions = [
+var fs = require("fs");
+var inquirer = require("inquirer");
 
+const questions = [
+  {
+    type: "input",
+    message: "What is your user name?",
+    name: "username",
+  },
 ];
 
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {}
 
-function init() {
-
-}
+function init() {}
 
 init();
+
+inquirer.prompt(questions).then(function (response) {
+  console.log(response);
+  fs.writeFile("README.md", [response.name], function (err) {
+    // Try to make the README.md all capitals
+    // process.argv is what we keep track of in the command line and what we type in the terminal
+
+    if (err) {
+      return console.log(err);
+    }
+  });
+});
